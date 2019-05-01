90a26998f9ee08b4ff35af0246c518aba9e1c9a4
Dmitry Povyaznikov <d.podvyaznikov@analysiscenter.org>
2019-05-01 13:00:34 +0300

    Solution for 944

---
+++ 944.delete-columns-to-make-sorted.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        ans = 0
+        for col in zip(*A):
+            if any(col[i] > col[i+1] for i in xrange(len(col) - 1)):
+                ans += 1
+        return ans
