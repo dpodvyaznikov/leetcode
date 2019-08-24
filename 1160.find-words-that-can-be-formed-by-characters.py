46affb36935eec8aa69993eb8ef675cbec7c715e
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2019-08-24 20:30:43 +0300

    Add solution for 1160

---
+++ 1160.find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countCharacters(self, words, chars):
+        """
+        You are given an array of strings words and a string chars.
+        A string is good if it can be formed by characters from chars (each character can only be used once).
+        Return the sum of lengths of all good strings in words.
+
+        Example 1:
+        Input: words = ["cat","bt","hat","tree"], chars = "atach"
+        Output: 6
+        Explanation:
+        The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+        Example 2:
+        Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+        Output: 10
+        Explanation:
+        The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+
+        Note:
+        1 <= words.length <= 1000
+        1 <= words[i].length, chars.length <= 100
+        All strings contain lowercase English letters only.
+        """
+        good_words = []
+
+        for word in words:
+            skip_word = False
+            for char in word:
+                if word.count(char) > chars.count(char):
+                    skip_word = True
+                    break
+            if skip_word:
+                continue
+            good_words.append(word)
+
+        return sum([len(w) for w in good_words])
